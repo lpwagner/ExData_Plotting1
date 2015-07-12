a27975c3e455f1071e43addaa522e39a197eac0d
Loren <lpwagner@uwm.edu>
2015-07-12 15:32:23 -0500

    plot1.png

---
+++ plot2.R
@@ -0,0 +1,14 @@
+plot2=function(data=NULL){
+    if (is.null(data)){
+        url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(url,"EPC.zip")
+        unzip("EPC.zip",exdir="EPC")
+        HPC<- read.csv("~/EPC/household_power_consumption.txt", sep=";", na.strings="?", stringsAsFactors=FALSE)
+        HPC.data=subset(HPC,HPC[,1]=="1/2/2007"|Date=="2/2/2007")
+        attach(HPC.data)
+    }else{attach(data)}
+    
+    plot(Global_active_power,type = "l",ylab = "Global Active Power (kilowatts)",xlab = "",xaxt="n")
+    axis(1,at=c(0,1500,2872),labels=c("Thu","Fri","Sat"))
+       
+}
